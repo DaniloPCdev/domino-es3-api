99e464243415bc1264c68489ff11ce8b0197e81c
DaniloPCdev <danilopcdev@gmail.com>
2021-11-06 21:44:30 -0300

    feat: primeiro commit

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const express = require('express');
 
 const app = express();
+const PORT = process.env.PORT || 8080
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.json({ "message": "[WIP] Server is running." });
+    res.json({ 'message': '[WIP] Server is running.' });
 });
-
-let PORT = 8080
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
